3ca0bdbbd4366d5525f3d7cc283b2686570fcc76
se-staekva-01 <se-staekva-01@SE-DEVPC1032>
2016-11-21 11:46:55 +0100

    hh

--- src/App.cs
+++ src/App.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using Nancy.Hosting.Self;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
@@ -35,11 +37,106 @@ namespace Nancy.Simple
 
 		static void Main (string[] args)
 		{
+		    //test();
+		    //return;
 			Host = new NancyHost (CurrentAddress);
 			Host.Start ();
 			Console.WriteLine ("Nancy is started and listening on {0}...", CurrentAddress);
 			while (Console.ReadLine () != "quit");
 			Host.Stop ();
 		}
+
+	    private static void test()
+	    {
+	        var gs = JObject.Parse(@"{
+""tournament_id"":""550d1d68cd7bd10003000003"",     // Id of the current tournament
+
+    ""game_id"":""550da1cb2d909006e90004b1"",           // Id of the current sit'n'go game. You can use this to link a
+                                                    // sequence of game states together for logging purposes, or to
+                                                    // make sure that the same strategy is played for an entire game
+
+    ""round"":0,                                      // Index of the current round within a sit'n'go
+
+    ""bet_index"":0,                                  // Index of the betting opportunity within a round
+
+    ""small_blind"": 10,                              // The small blind in the current round. The big blind is twice the
+                                                    //     small blind
+
+    ""current_buy_in"": 320,                          // The amount of the largest current bet from any one player
+
+    ""pot"": 400,                                     // The size of the pot (sum of the player bets)
+
+    ""minimum_raise"": 240,                           // Minimum raise amount. To raise you have to return at least:
+                                                    //     current_buy_in - players[in_action][bet] + minimum_raise
+
+    ""dealer"": 1,                                    // The index of the player on the dealer button in this round
+                                                    //     The first player is (dealer+1)%(players.length)
+
+    ""orbits"": 7,                                    // Number of orbits completed. (The number of times the dealer
+                                                    //     button returned to the same player.)
+
+    ""in_action"": 1,                                 // The index of your player, in the players array
+
+    ""players"": [                                    // An array of the players. The order stays the same during the
+        {                                           
+            ""id"": 0,                               
+
+            ""name"": ""Albert"",                     
+
+            ""status"": ""active"",                    
+            ""version"": ""Default random player"", 
+
+            ""stack"": 1010,                        
+                                                    
+
+            ""bet"": 320                             
+        },
+        {
+            ""id"": 1,                              
+            ""name"": ""Bob"",
+            ""status"": ""active"",
+            ""version"": ""Default random player"",
+            ""stack"": 1590,
+            ""bet"": 80,
+            ""hole_cards"": [                       
+                                                    
+                {
+                    ""rank"": ""K"",                
+                    ""suit"": ""hearts""            
+                },
+                {
+                    ""rank"": ""K"",
+                    ""suit"": ""spades""
+                }
+            ]
+        },
+        {
+            ""id"": 2,
+            ""name"": ""Chuck"",
+            ""status"": ""out"",
+            ""version"": ""Default random player"",
+            ""stack"": 0,
+            ""bet"": 0
+        }
+    ],
+    ""community_cards"": [                            
+        {
+            ""rank"": ""4"",
+            ""suit"": ""spades""
+        },
+        {
+            ""rank"": ""A"",
+            ""suit"": ""hearts""
+        },
+        {
+            ""rank"": ""6"",
+            ""suit"": ""clubs""
+        }
+    ]
+}");
+	        Console.WriteLine(gs);
+	        PokerPlayer.BetRequest(gs);
+
+	    }
 	}
 }
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,26 +1,58 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
-	public static class PokerPlayer
+    public static class JsonExtensions
+    {
+        public static List<JToken> FindTokens(this JToken containerToken, string name)
+        {
+            List<JToken> matches = new List<JToken>();
+            FindTokens(containerToken, name, matches);
+            return matches;
+        }
+
+        private static void FindTokens(JToken containerToken, string name, List<JToken> matches)
+        {
+            if (containerToken.Type == JTokenType.Object)
+            {
+                foreach (JProperty child in containerToken.Children<JProperty>())
+                {
+                    if (child.Name == name)
+                    {
+                        matches.Add(child.Value);
+                    }
+                    FindTokens(child.Value, name, matches);
+                }
+            }
+            else if (containerToken.Type == JTokenType.Array)
+            {
+                foreach (JToken child in containerToken.Children())
+                {
+                    FindTokens(child, name, matches);
+                }
+            }
+        }
+    }
+    public static class PokerPlayer
 	{
 		public static readonly string VERSION = "All in on pairs";
 
 		public static int BetRequest(JObject gameState)
 		{
-		    var players = gameState.SelectToken("players");
-		    foreach (var player in players)
-		    {
-		        var holeCards = player.SelectToken("hole_cards");
-		        if (holeCards != null)
-		        {
-		            var c1 = holeCards.SelectToken("rank")[0].Value<int>();
-                    var c2 = holeCards.SelectToken("rank")[1].Value<int>();
-		            if (c1 == c2)
-		                return 10000;
-		        }
+
+		    var holeCards = gameState.FindTokens("hole_cards").ToList();
+            var c1 = holeCards[0].First();
+            var c2 = c1.Next;
+
+            if (c1.Value<string>("rank") == c2.Value<string>("rank"))
+            {
+                return 10000;
             }
-		    return 0;
+
+            return 0;
 		}
 
 		public static void ShowDown(JObject gameState)
